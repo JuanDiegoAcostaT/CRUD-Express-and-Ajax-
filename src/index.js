c49f3bdcc04193894cbbdaaa86548fc847fcf162
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-04-23 15:47:05 -0500

    Adding public Folder and Making an easy template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+
+//Settings
+
+app.set('port', process.env.PORT || 3000);
+
+
+
+//Static Files
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
